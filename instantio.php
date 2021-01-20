8f7ff2b672ba92eb131d18becdd58be20172801b
Shuvo <aktarujjman@gmail.com>
2021-01-20 13:34:12 +0600

    fix: reload

--- instantio.php
+++ instantio.php
@@ -34,9 +34,9 @@ include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 /**
  * Loading Text Domain
  */
-add_action('plugins_loaded', 'wooinstant_plugin_loaded_action', 10, 2);
+add_action('plugins_loaded', 'instantio_lite_plugin_loaded_action', 10, 2);
 
-function wooinstant_plugin_loaded_action() {
+function instantio_lite_plugin_loaded_action() {
 	//Internationalization
 	load_plugin_textdomain( 'instantio', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
@@ -53,7 +53,7 @@ function wooinstant_plugin_loaded_action() {
  *	Enqueue Instantio scripts
  *
  */
-function wooinstant_enqueue_scripts(){
+function instantio_lite_enqueue_scripts(){
 
 	$INSTANTIO_VERSION = current_time('timestamp');
 
@@ -68,66 +68,8 @@ function wooinstant_enqueue_scripts(){
 	    )
     );
 
-	/**
-	 * Handle WC frontend scripts
-	 *
-	 * @package WooCommerce/Classes
-	 * @version 2.3.0
-	 * http://woocommerce.wp-a2z.org/oik_api/wc_frontend_scriptsget_script_data/
-	 */
-	//remove_action('wp_head', array($GLOBALS['woocommerce'], 'generator'));
-
-	//first check that woo exists to prevent fatal errors
-	if( function_exists('is_woocommerce') ) {
-		$suffix               = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? : '.min';
-		$lightbox_en          = 'yes' === get_option( 'woocommerce_enable_lightbox' );
-		$ajax_cart_en         = 'yes' === get_option( 'woocommerce_enable_ajax_add_to_cart' );
-		$assets_path          = str_replace( array( 'http:', 'https:' ), '', WC()->plugin_url() ) . '/assets/';
-		$frontend_script_path = $assets_path . 'js/frontend/';
-
-		//global $wp_scripts;
-    	//$wp_scripts->registered[ 'wc-checkout' ]->src = plugin_dir_url( __FILE__ ) . 'assets/js/checkout.js';
-
-		wp_enqueue_script( 'wc-cart', $frontend_script_path . 'cart' . $suffix . '.js', array( 'jquery', 'wc-country-select', 'wc-address-i18n' ) );
-
-	    wp_localize_script('wc-cart', 'wc_cart_params', apply_filters('wc_cart_params', array(
-			'ajax_url' => WC()->ajax_url() ,
-			'wc_ajax_url' => WC_AJAX::get_endpoint(' %%endpoint%%') ,
-			'ajax_loader_url' => apply_filters('woocommerce_ajax_loader_url', $assets_path . 'images / ajax - loader@2x . gif') ,
-			'update_shipping_method_nonce' => wp_create_nonce('update-shipping-method') ,
-		)));
-
-		wp_enqueue_script( 'wc-checkout', $frontend_script_path . 'checkout' . $suffix . '.js', array( 'jquery', 'wc-address-i18n' ) );
-
-	    wp_localize_script('wc-checkout', 'wc_checkout_params', apply_filters('wc_checkout_params', array(
-			'ajax_url'                  => WC()->ajax_url(),
-			'wc_ajax_url'               => WC_AJAX::get_endpoint( '%%endpoint%%' ),
-			'update_order_review_nonce' => wp_create_nonce( 'update-order-review' ),
-			'apply_coupon_nonce'        => wp_create_nonce( 'apply-coupon' ),
-			'remove_coupon_nonce'       => wp_create_nonce( 'remove-coupon' ),
-			'option_guest_checkout'     => get_option( 'woocommerce_enable_guest_checkout' ),
-			'checkout_url'              => WC_AJAX::get_endpoint( 'checkout' ),
-			'is_checkout'               => is_checkout() && empty( $wp->query_vars['order-pay'] ) && ! isset( $wp->query_vars['order-received'] ) ? 1 : 0,
-			'debug_mode'                => defined( 'WP_DEBUG' ) && WP_DEBUG,
-			'i18n_checkout_error'       => esc_attr__( 'Error processing checkout. Please try again.', 'woocommerce' ),
-		)));
-
-		wp_enqueue_script( 'wc-add-to-cart-variation', $frontend_script_path . 'add-to-cart-variation' . $suffix . '.js', array( 'jquery', 'wp-util', 'jquery-blockui' ) );
-
-	    wp_localize_script('wc-add-to-cart-variation', 'wc_add_to_cart_variation_params', apply_filters('wc_add_to_cart_variation_params', array(
-			'wc_ajax_url'                      => WC_AJAX::get_endpoint( '%%endpoint%%' ),
-			'i18n_no_matching_variations_text' => esc_attr__( 'Sorry, no products matched your selection. Please choose a different combination.', 'woocommerce' ),
-			'i18n_make_a_selection_text'       => esc_attr__( 'Please select some product options before adding this product to your cart.', 'woocommerce' ),
-			'i18n_unavailable_text'            => esc_attr__( 'Sorry, this product is unavailable. Please choose a different combination.', 'woocommerce' ),
-		)));
-
-		wp_enqueue_style('select2');
-		wp_enqueue_script('select2');
-		wp_enqueue_script( 'wc-country-select' );
-
-	}
 }
-add_filter( 'wp_enqueue_scripts', 'wooinstant_enqueue_scripts', 200 ,2 ); // Giving high priority
+add_filter( 'wp_enqueue_scripts', 'instantio_lite_enqueue_scripts' );
 
 /**
  *	Instantio Menu Layout
@@ -139,18 +81,6 @@ require_once( dirname( __FILE__ ) . '/inc/layout.php' );
  */
 require_once( dirname( __FILE__ ) . '/inc/functions.php' );
 
-/**
- *	Plugin activation hook
- */
-function wooinstant_activation_redirect( $plugin ) {
-	if( $plugin == plugin_basename( __FILE__ ) ) {
-	    // redirect option page after installed
-	    wp_redirect( admin_url( 'admin.php?page=_woinstant' ) );
-	    exit;
-	}
-}
-add_action( 'activated_plugin', 'wooinstant_activation_redirect' );
-
 
 /**
  * Add plugin action links.
@@ -158,10 +88,10 @@ add_action( 'activated_plugin', 'wooinstant_activation_redirect' );
  * @since 1.0.0
  * @version 4.0.0
  */
-function wi_plugin_action_links( $links ) {
+function instantio_lite_plugin_action_links( $links ) {
 	$plugin_links = array(
-		'<a href="admin.php?page=_woinstant">' . esc_html__( 'Settings', 'wooinstant' ) . '</a>',
+		'<a href="admin.php?page=_instantio_lite">' . esc_html__( 'Settings', 'instantio' ) . '</a>',
 	);
 	return array_merge( $plugin_links, $links );
 }
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wi_plugin_action_links' );
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'instantio_lite_plugin_action_links' );
