7d31beca8bf221fe4fc558959b36d08a476d6e11
Shuvo <aktarujjman@gmail.com>
2021-01-23 13:00:29 +0600

    lite folder added

---
+++ instantio-lite/assets/js/common.js
@@ -0,0 +1,183 @@
+(function($) {
+    'use strict';
+
+    jQuery(document).ready(function() {
+
+        //Add To Cart Fly Effect
+        $(document).on('click', '.add_to_cart_button:not(.product_type_variable), .single_add_to_cart_button:not(.disabled)', function() {
+
+            $('body').append('<div id="wi-cart-fly">'+instantio_ajax_params.cart_icon+'</div>');
+
+            var endPos = $("#wi-toggler").offset();
+            var startPos = $(this).offset();
+
+            $('#wi-cart-fly').css({
+                'top': startPos.top + 'px',
+                'left': startPos.left + 'px'
+            })
+            .animate({
+                opacity: 1,
+                top: endPos.top,
+                left: endPos.left
+            }, 1500, function() {
+                $(this).css({
+                    'opacity': '0',
+                    'z-index': '0'
+                });
+                $(this).detach();
+            });
+        });
+
+        // Added to cart JS
+        // For custom trigger use https://pastebin.com/Y2QpTm1b
+        $(document.body).on('added_to_cart', function() {
+            setTimeout(function(){
+                if ( wiCartTotal > 0 ) {
+                    $('#wi-toggler').addClass( 'hascart' );
+                } else {
+                    $('#wi-toggler').removeClass( 'hascart' );
+                }
+            },300);
+        });
+
+    });
+
+})(jQuery);
+
+// Toggle Panel
+(function($) {
+    'use strict';
+
+    jQuery(document).ready(function() {
+
+        //Nav Toggler
+        $(document).on('click', '#wi-toggler, .added_to_cart', function(e) {
+            //e.preventDefault();
+
+            var targetClass = $('#wi-toggler, .added_to_cart');
+
+            if (targetClass.hasClass('open')) {
+                targetClass.removeClass('open');
+                $('.wi-container').removeClass('panel-open');
+                $('html').removeClass('wi-panel-open');
+            } else {
+                targetClass.addClass('open');
+                $('.wi-container').addClass('panel-open');
+                $('html').addClass('wi-panel-open');
+            }
+
+        });
+
+        //Collapse Nav if click on body
+        $('html').on('click', function (e) {
+            if (!$('#wi-toggler, .added_to_cart, .add_to_cart_button').is(e.target) && $('#wi-toggler, .added_to_cart, .add_to_cart_button').has(e.target).length === 0 && !$('.wi-inner').is(e.target) && $('.wi-inner').has(e.target).length === 0) {
+                $('#wi-toggler, .added_to_cart').removeClass('open');
+                $('.wi-container').removeClass('panel-open');
+                $('html').removeClass('wi-panel-open');
+            }
+        });
+
+    });
+
+})(jQuery);
+
+// For quick view
+jQuery( function( $ ){
+    $(document).ready(function(){
+
+        // Add Quick View Panel DIV to body
+        $(document.body).append('<div class="wi-quick-view"></div>');
+
+        // Close Quick View Panel
+        $(document).on('click', '.wi-quick-view .close', function(e) {
+            $(this).parent().fadeOut( 300 );
+        })
+
+        // Variable Product Quick View Ajax on Click
+        $(document).on('click', '.product_type_variable', function(e) {
+            e.preventDefault();
+
+            var $this = $(this),
+                cartPos = $this.offset(),
+                product_id = $this.data('product_id');
+
+            $('.wi-quick-view').css({
+                'top': parseInt( cartPos.top ) + parseInt( 45 ) + 'px',
+                'left': cartPos.left + 'px'
+            })
+
+            $.ajax({
+                type: 'post',
+                url: instantio_ajax_params.wi_ajax_url,
+                data: {
+                    action: 'wi_variable_product_quick_view',
+                    security: instantio_ajax_params.wi_ajax_nonce,
+                    product_id: product_id,
+                },
+                beforeSend: function(data){
+                    $this.addClass('loading');
+                    $('.wi-quick-view').block();
+                },
+                success: function(data){
+                    $this.removeClass('loading');
+                    $('.wi-quick-view').fadeIn( 300 ).html(data).prepend('<span class="close"></span>');
+                },
+                error: function(data){
+                    console.log(data);
+                },
+            });
+        });
+
+    });
+});
+
+// Single Product Ajax Cart
+(function($) {
+    'use strict';
+    jQuery(document).ready(function() {
+
+        $(document).on('click', '.single_add_to_cart_button:not(.disabled)', function (e) {
+            e.preventDefault();
+
+            var thisbutton = $(this),
+                cart_form = thisbutton.closest('form.cart'),
+                id = thisbutton.val(),
+                product_qty = cart_form.find('input[name=quantity]').val() || 1,
+                product_id = cart_form.find('input[name=product_id]').val() || id,
+                variation_id = cart_form.find('input[name=variation_id]').val() || 0;
+
+            var data = {
+                action: 'wi_single_ajax_add_to_cart',
+                product_id: product_id,
+                product_sku: '',
+                quantity: product_qty,
+                variation_id: variation_id,
+            };
+
+            $(document.body).trigger('adding_to_cart', [thisbutton, data]);
+
+            $.ajax({
+                type: 'post',
+                url: instantio_ajax_params.wi_ajax_url,
+                data: data,
+                beforeSend: function (response) {
+                    thisbutton.removeClass('added').addClass('loading');
+                },
+                complete: function (response) {
+                    thisbutton.addClass('added').removeClass('loading');
+                },
+                success: function (response) {
+                    if (response.error & response.product_url) {
+                        window.location = response.product_url;
+                        return;
+                    } else {
+                        $(document.body).trigger('added_to_cart', [response.fragments, response.cart_hash, thisbutton]);
+                    }
+                },
+            });
+
+            return false;
+        });
+
+    });
+})(jQuery);
---
+++ instantio-lite/instantio.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Plugin Name: Instantio Lite
+ * Plugin URI: https://themefic.com/plugins/wooinstant/
+ * Github Plugin URI: https://github.com/themefic/instantio
+ * Description: Instantio converts multistep checkout into WooCommerce One Page Checkout. The Whole WooCommerce checkout process would take only 10-15 seconds. Yes, you heard it right! Only 10-15 Seconds! All your customer have to do is “Add to Cart”, a popup/cart drawer will appear with the cart view. Your customer can direct checkout WooCommerce based store and order from that single window!. No Page Reload whatsoever!
+ * Author: BootPeople
+ * Text Domain: instantio
+ * Domain Path: /lang/
+ * Author URI: https://psdtowpservice.com
+ * Tags: instantio,responsive,woocommerce
+ * Version: 1.0.0
+ * WC tested up to: 4.9.0
+ */
+
+// don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+// Define INSTANTIO_VERSION.
+if ( ! defined( 'INSTANTIO_VERSION' ) ) {
+	define( 'INSTANTIO_VERSION', '1.0.1' );
+}
+
+/**
+ * Including Plugin file for security
+ * Include_once
+ *
+ * @since 1.0.0
+ */
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+/**
+ * Loading Text Domain
+ */
+add_action('plugins_loaded', 'instantio_lite_plugin_loaded_action', 10, 2);
+function instantio_lite_plugin_loaded_action() {
+	//Internationalization
+	load_plugin_textdomain( 'instantio', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+
+}
+
+
+/**
+ *	Instantio Functions
+ */
+require_once( dirname( __FILE__ ) . '/inc/functions.php' );
+
+
+/**
+ *	Enqueue Instantio scripts
+ *
+ */
+function instantio_lite_enqueue_scripts(){
+
+	$INSTANTIO_VERSION = current_time('timestamp');
+
+	wp_enqueue_style('instantio-common-styles', plugin_dir_url( __FILE__ ) . 'assets/css/common.css','', $INSTANTIO_VERSION );
+	wp_enqueue_script( 'instantio-common-scripts', plugin_dir_url( __FILE__ ) . 'assets/js/common.js', array('jquery'), $INSTANTIO_VERSION, true );
+
+
+	wp_localize_script( 'instantio-common-scripts', 'instantio_ajax_params',
+		array(
+	        'wi_ajax_nonce' => wp_create_nonce( 'wi_ajax_nonce' ),
+	        'wi_ajax_url' => admin_url( 'admin-ajax.php' ),
+	        'cart_icon' => instantio_lite_get_svg_icon('shopping_cart'),
+	    )
+    );
+
+
+	/**
+	 * Handle WC frontend scripts
+	 *
+	 * @package WooCommerce/Classes
+	 * @version 2.3.0
+	 * http://woocommerce.wp-a2z.org/oik_api/wc_frontend_scriptsget_script_data/
+	 */
+
+	//first check that woo exists to prevent fatal errors
+	if( function_exists('is_woocommerce') ) {
+		$suffix               = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? : '.min';
+		$assets_path          = str_replace( array( 'http:', 'https:' ), '', WC()->plugin_url() ) . '/assets/';
+		$frontend_script_path = $assets_path . 'js/frontend/';
+
+		wp_enqueue_script( 'wc-add-to-cart-variation', $frontend_script_path . 'add-to-cart-variation' . $suffix . '.js', array( 'jquery', 'wp-util', 'jquery-blockui' ) );
+
+	    wp_localize_script('wc-add-to-cart-variation', 'wc_add_to_cart_variation_params', apply_filters('wc_add_to_cart_variation_params', array(
+			'wc_ajax_url'                      => WC_AJAX::get_endpoint( '%%endpoint%%' ),
+			'i18n_no_matching_variations_text' => esc_attr__( 'Sorry, no products matched your selection. Please choose a different combination.', 'woocommerce' ),
+			'i18n_make_a_selection_text'       => esc_attr__( 'Please select some product options before adding this product to your cart.', 'woocommerce' ),
+			'i18n_unavailable_text'            => esc_attr__( 'Sorry, this product is unavailable. Please choose a different combination.', 'woocommerce' ),
+		)));
+
+	}
+
+}
+add_filter( 'wp_enqueue_scripts', 'instantio_lite_enqueue_scripts' );
+
+
+/**
+ * Add plugin action links.
+ *
+ * @since 1.0.0
+ * @version 4.0.0
+ */
+function instantio_lite_plugin_action_links( $links ) {
+	$plugin_links = array(
+		'<a href="admin.php?page=_instantio_lite">' . esc_html__( 'Settings', 'instantio' ) . '</a>',
+	);
+	return array_merge( $plugin_links, $links );
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'instantio_lite_plugin_action_links' );
